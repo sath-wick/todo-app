f2dc9802f446abd225938e4fc417ab1a778d1b87
Sathwik Ponna <81036216+sath-wick@users.noreply.github.com>
2025-05-20 10:44:21 +0530

    Updated db.js
    Added ssl to reject unauthorized access

--- server/db.js
+++ server/db.js
@@ -2,6 +2,7 @@ const {Pool} = require('pg')
 require('dotenv').config()
 const pool = new Pool({
     connectionString: process.env.DB_CON_STR
+    ssl : { rejectUnauthorized: false }
 })
 
 module.exports = pool
