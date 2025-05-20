5a8cef5b757ddc951b3700be788bab945c78c40a
Sathwik Ponna <81036216+sath-wick@users.noreply.github.com>
2025-05-20 19:38:59 +0530

    Update db.js
    added ssl

--- server/db.js
+++ server/db.js
@@ -1,7 +1,8 @@
 const {Pool} = require('pg')
 require('dotenv').config()
 const pool = new Pool({
-    connectionString: process.env.DB_CON_STR
+    connectionString: process.env.DB_CON_STR,
+    ssl : { rejectUnauthorized: false }
 })
 
 module.exports = pool
