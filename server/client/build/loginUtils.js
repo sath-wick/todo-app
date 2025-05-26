b255927e1c3b1866659bd688392901762731f8e7
sath-wick <sathwik.ponna161@gmail.com>
2025-05-26 11:24:47 +0530

    built client and added home page and login logic (incomplete)

---
+++ server/client/build/loginUtils.js
@@ -0,0 +1,11 @@
+const { apiPOST } = require('../utils/api')
+
+export default async function validateLogin(username, password){
+    if (!username || !password) return
+    try {
+        const body = {username, password}
+        const jsonData = await apiPOST('/login-user',body)
+    } catch (err) {
+        console.error(err.message);
+    }
+} 
